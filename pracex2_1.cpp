838d9b31cfc0ff3910885d99d9b05552c0cb34d7
lab314-55@labeng.local <lab314-55@labeng.local>
2019-03-01 15:13:56 +0700

    1

--- pracex2_1.cpp
+++ pracex2_1.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+template <typename T>
+int countIf(T data[],int size,T key){
+    int count=0;
+	for(int i=0;i<size;i++){
+        if(data[i]==key){
+            count+=1;
+        }
+        
+    }
+    return count;
+
+}
+
+
+int main(){
+	int a[] = {1,0,2,3,4,0,1,0,0,5,1,9};
+	cout << countIf(a,12,0) << "\n";
+	cout << countIf(a,12,8) << "\n";
+	
+	char b[] = "CPECMU";
+	cout << countIf(b,6,'C') << "\n";
+	cout << countIf(b,6,'E') << "\n";
+	
+	return 0;
+}
