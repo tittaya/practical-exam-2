c8461e6ae34a579129f3bbb4408d07a36aa379e6
lab314-55@labeng.local <lab314-55@labeng.local>
2019-03-01 16:22:04 +0700

    2

--- pracex2_2.cpp
+++ pracex2_2.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+void sort(float x[], int N){
+	for(int i = 1; i < N; i++){
+		for(int j = i; j > 0; j--){
+			if(x[j] < x[j-1]){
+				float temp = x[j];
+				x[j] = x[j-1];
+				x[j-1] = temp;
+			}else{
+				break;
+			} 
+		}
+	}
+}
+
+int main(){
+	
+    ofstream dest("result.txt");
+    ifstream source("scorce1.txt");
+    string line;
+    float A[20];
+	while(getline(source,line)){
+        for(int i=0;i<20;i++){
+            source >> A[i];
+        }
+    }
+    
+    sort(A,20);
+    for(int j=0;j<20;j++){
+        dest << A[j] << "\n";
+    }
+	
+	return 0;
+}
 
